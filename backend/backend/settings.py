1edd40b8a123c2e452c2213002738873ca0cdfbb
Liyakhat~ <liyakhat@gmail.com>
2025-09-09 23:10:28 +0530

    again modified

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,11 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -22,10 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7gk3ex@4+4*&u)1v_(-6m*ll&ry%y2*9dk#^#py&jdm5m1)o%*'
 
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["protofolio-r34i.onrender.com", "127.0.0.1", "localhost"]
+
 
 
 # Application definition
